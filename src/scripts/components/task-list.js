a9f11ba1de8abdcd7a8ccb9e231247fab05bd0a2
yohira0616 <yukito.sys.so@gmail.com>
2016-04-30 17:20:29 +0900

    イベントのバインド

--- src/scripts/components/task-list.js
+++ src/scripts/components/task-list.js
@@ -2,15 +2,27 @@ class TaskList {
 
   constructor($http) {
     this.$http = $http;
-    this.data=[];
+    this.data = [];
 
     this.initialize();
   };
 
-  initialize(){
-
+  initialize() {
+    let mock = {
+      taskId: -1,
+      taskName: 'sample'
+    };
+    let mock2 = {
+      taskId: 0,
+      taskName: 'NullTask'
+    };
+    this.data.push(mock);
+    this.data.push(mock2);
   }
 
+  taskDone(taskId) {
+    console.log(taskId + '完了');
+  }
 }
 
 
@@ -20,7 +32,7 @@ angular.module('app')
       restrict: 'E',
       controller: TaskList,
       controllerAs: 'taskList',
-      templateUrl:'task-list.html',
+      templateUrl: 'task-list.html',
       scope: {}
     }
   });
